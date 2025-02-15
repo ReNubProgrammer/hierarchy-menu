a543e38de411fb6f6a49bf0e3f3496d9601ae4f7
ReNubProgrammer <insomnia.knights.00@gmail.com>
2025-02-15 17:30:30 +0700

    add client

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import  NextServer  from 'next';
+import { NextFunction, Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const dev = process.env.NODE_ENV !== 'production';
+  const nextApp = NextServer({ dev });
+  const handle = nextApp.getRequestHandler();
+
+  await nextApp.prepare();
+
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    handle(req, res);
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
